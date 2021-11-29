9f4972017cb7c59346a4baab5e022519adbefab8
lee <708851221@qq.com>
2021-11-29 16:35:17 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '李与李语桐',
+  description: '爸爸的奶粉奋斗笔记',
+  head: [
+    ['link', {rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico`}]
+  ],
+  host: '0.0.0.0',
+  port: 10130
+}
+
+
+
+
